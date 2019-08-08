270544061e227262a3f57a918967b7d056bb6902
Iago Olímpio Frota <iagofrota10@gmail.com>
2019-08-07 21:42:01 -0300

    component PageHeader

--- frontend/src/about/about.jsx
+++ frontend/src/about/about.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
+import PageHeader from '../template/pageHeader'
 
 export default props => (
     <div>
-        <h1>Sobre</h1>
+        <PageHeader name='Sobre' small='Nós'></PageHeader>
+
+        <h2>Nossa História</h2>
+        <p>Dias de luta, dias de glória</p>
+        <h2>Missão e Visão</h2>
+        <p>Dias de luta, dias de glória</p>
+        <h2>Imprensa</h2>
+        <p>Dias de luta, dias de glória</p>
     </div>
 )
